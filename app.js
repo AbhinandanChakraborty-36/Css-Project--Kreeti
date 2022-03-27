7343d868215acb7b9cbf8ad19b98f33f5d715c76
Abhinandan Chakraborty <abchaks99@gmail.com>
2022-03-27 13:30:44 +0530

    Third Commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const toggler=document.getElementById('toggleBtn')
 const it=document.getElementById('item1')
 const nav=document.getElementById('nav')
 const search=document.getElementById('search-bar')
+const met=document.getElementById('main-img')
 let i=false
 
 console.log(screen.width)
@@ -9,7 +10,7 @@ console.log(screen.width)
 toggler.addEventListener("click",()=>{
     it.classList.toggle('active')
     search.classList.toggle('active')
-
+    met.classList.toggle('het')
 //    if(i===false && screen.width <= 480)
 //    {
 //        nav.style.backgroundColor="black"
